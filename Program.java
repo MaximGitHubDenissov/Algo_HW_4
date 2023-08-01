c51b6650fa39eddaa2d8cce5c97dc1440a349fa9
Maxim Denissov <DM7894289@gmail.com>
2023-08-01 15:34:36 +0600

    Add HW_4

---
+++ Program.java
@@ -0,0 +1,17 @@
+package HW_4;
+
+public class Program {
+    public static void main(String[] args) {
+        
+        HashMap telbook = new HashMap<>(5);
+        telbook.put("111", "Andrey");
+        telbook.put("222", "Max");
+        telbook.put("333", "Nick");
+        telbook.put("444", "Lena");
+        telbook.put("555", "Mark");
+        telbook.put("777", "Olya");
+        telbook.enumeration();
+
+    }
+    
+}
